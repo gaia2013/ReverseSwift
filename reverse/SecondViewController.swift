c69f615475aca22d1ecf541e1771fc3118287593
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-08-13 00:06:47 +0900

    015.UIViewControllerで画面遷移する

--- reverse/SecondViewController.swift
+++ reverse/SecondViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  reverse
 //
 //  Created by 山口仁志 on 2018/08/12.
@@ -8,44 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SecondViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 背景色をGreenに設定する
-        self.view.backgroundColor = UIColor.green
         
-        // ボタンを生成する
+        // 背景色を設定
+        self.view.backgroundColor = UIColor.blue
+        
+        // ボタンを生成
         let nextButton: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 120, height: 50))
         nextButton.backgroundColor = UIColor.red
         nextButton.layer.masksToBounds = true
         nextButton.setTitle("Next", for: .normal)
         nextButton.layer.cornerRadius = 20.0
-        nextButton.layer.position = CGPoint(x: self.view.bounds.width/2, y:self.view.bounds.height-50)
-        nextButton.addTarget(self, action: #selector(onClickMyButton(sender:)), for: UIControlEvents.touchUpInside)
+        nextButton.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height-50)
+        nextButton.addTarget(self, action: #selector(onClickMyButton(_:)), for: .touchUpInside)
         
         // ボタンを追加する
         self.view.addSubview(nextButton);
+        
     }
     
     // ボタンイベント
-    @objc func onClickMyButton(sender: UIButton){
+    @objc internal func onClickMyButton(_ sender: UIButton){
         
         // 遷移するViewを定義する
         let mySecondViewController: UIViewController = SecondViewController()
         
-        // アニメーションを設定する。
-        mySecondViewController.modalTransitionStyle = .partialCurl
+        // アニメーションを設定する
+        mySecondViewController.modalTransitionStyle = UIModalTransitionStyle.partialCurl
         
-        // Viewの移動する
+        // Viewの移動をする
         self.present(mySecondViewController, animated: true, completion: nil)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
-
