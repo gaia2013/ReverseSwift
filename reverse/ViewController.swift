0377d566d151a4c948f94ba65daea35fdc4b4cf2
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-08-13 09:13:01 +0900

    016.UIScrollViewDisplay

--- reverse/ViewController.swift
+++ reverse/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var myScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // ScrollViewを生成
+        myScrollView = UIScrollView()
+        
+        // ScrollViewの大きさを設定する
+        myScrollView.frame = self.view.frame
+        
+        // UIImageに画像を保存する
+        let myImage = UIImage(named: "temple.jpg")!
+        
+        // UIImageViewを生成する
+        let myImageView = UIImageView()
+        
+        // myImageViewのImageにmyImageを設定する
+        myImageView.image = myImage
+        
+        // frameの値を設定する
+        myImageView.frame = myScrollView.frame
+        
+        // 画像のアスペクト比を設定
+        myImageView.contentMode = UIViewContentMode.scaleAspectFill
+        
+        // ScrollViewにmyImageViewを追加する
+        myScrollView.addSubview(myImageView)
+        
+        // Scrollの高さを計算しておく
+        let scroll_height = myImage.size.height*(self.view.frame.width/myImage.size.width)
+        
+        // ScrollviewにcontentSizeを設定する
+        myScrollView.contentSize = CGSize(width:self.view.frame.width, height: scroll_height)
+        
+        // ViewにScrollViewをAddする
+        self.view.addSubview(myScrollView)
     }
 
     override func didReceiveMemoryWarning() {
