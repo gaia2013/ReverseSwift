a655ed984d1fb9c4c5d778a7a300bf1677183337
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2018-09-05 22:35:59 +0900

    057.TransmissionofImageintheUIImage

--- reverse/ViewController.swift
+++ reverse/ViewController.swift
@@ -8,16 +8,43 @@
 
 import UIKit
 
+extension UIImage{
+    
+    //Resizeのクラスメソッドを作る
+    class func ResizeUIImage(image : UIImage, width : CGFloat, height : CGFloat) -> UIImage! {
+        
+        // 指定された画像の大きさのコンテキストを用意
+        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+        
+        // コンテキストに画像を描画する
+        image.draw(in: CGRect(x:0, y:0, width:width, height: height))
+        
+        // コンテキストからUIImageを作る
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // コンテキストを閉じる
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        // 画像の設定
+        let myImage:UIImage = UIImage(named: "sample2.jpg")!
+        
+        // リサイズされたUIImageを指定して、UIImageViewを作る
+        let myImageView = UIImageView(image: UIImage.ResizeUIImage(image: myImage, width: self.view.frame.maxX, height: self.view.frame.maxY))
+        
+        // 透過する
+        myImageView.alpha = 0.3
+        
+        // viewにUIImageViewを追加
+        self.view.addSubview(myImageView)
     }
 
 
